48c90799e038035e94d92e7b2981054c1c08bfce
Nick Ng <nicholas.cj.ng@gmail.com>
2022-06-21 21:38:57 +1200

    changed game data a bit

--- src-common/game-types.ts
+++ src-common/game-types.ts
@@ -6,12 +6,32 @@ export interface LobbyGameState {
   state: "lobby";
 }
 
+export interface ProgramCard {
+  id: string;
+  action:
+    | "Move 1"
+    | "Move 2"
+    | "Move 3"
+    | "Back Up"
+    | "Rotate Right"
+    | "Rotate Left"
+    | "U-Turn";
+  priority: number;
+}
+
+export interface RobotDamage {
+  damagePoints: number;
+  lockedRegisters: number[];
+}
+
 export interface MainGameState {
   state: "main";
   seatOrder: string[];
-  chosenCardPlayers: string[];
-  fingerOnNose: string[];
-  cardMap: { [cardId: string]: string };
+  finishedProgrammingPlayers: string[];
+  poweringDownNextTurn: string[];
+  cardMap: { [cardId: string]: ProgramCard };
+  robotsDamage: { [playerId: string]: RobotDamage };
+  robotLives: { [playerId: string]: number };
 }
 
 export type OverGameState = Omit<MainGameState, "state"> & {
@@ -22,8 +42,8 @@ export type GameState = LobbyGameState | MainGameState | OverGameState;
 
 export interface OnePlayerSecrets {
   password: string;
-  chosenCard?: string;
-  cardsInHand?: string[];
+  programRegisters: string[];
+  cardsInHand: string[];
 }
 
 export interface PlayerSecrets {
--- src-front/app/practice/index.tsx
+++ src-front/app/practice/index.tsx
@@ -1,8 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+
+import { PlayerGameData } from "../../../dist-common/game-types";
 
 const StyledPractice = styled.div``;
 
+const defaultGameData: PlayerGameData = {
+  id: "1",
+  shortId: "1",
+  host: "player-1's-uuid",
+  maxPlayers: 26,
+  players: [{ id: "player-1's-uuid", name: "You" }],
+  gameSettings: {
+    cardsPerPlayer: 4,
+  },
+  yourSecrets: {
+    password: "asdf",
+    programRegisters: [],
+    cardsInHand: [
+      "card-uuid-01",
+      "card-uuid-02",
+      "card-uuid-03",
+      "card-uuid-04",
+      "card-uuid-05",
+      "card-uuid-06",
+      "card-uuid-07",
+    ],
+  },
+  gameState: {
+    state: "main",
+    seatOrder: ["player-1's-uuid"],
+    finishedProgrammingPlayers: [],
+    poweringDownNextTurn: [],
+    robotsDamage: {
+      "player-1's-uuid": { damagePoints: 0, lockedRegisters: [] },
+    },
+    robotLives: { "player-1's-uuid": 3 },
+    cardMap: {
+      "card-uuid-01": {
+        id: "card-uuid-01",
+        action: "Move 1",
+        priority: 4,
+      },
+      "card-uuid-02": {
+        id: "card-uuid-02",
+        action: "Move 2",
+        priority: 5,
+      },
+      "card-uuid-03": {
+        id: "card-uuid-03",
+        action: "Move 3",
+        priority: 6,
+      },
+      "card-uuid-04": {
+        id: "card-uuid-04",
+        action: "Back Up",
+        priority: 3,
+      },
+      "card-uuid-05": {
+        id: "card-uuid-05",
+        action: "Rotate Right",
+        priority: 2,
+      },
+      "card-uuid-06": {
+        id: "card-uuid-06",
+        action: "Rotate Left",
+        priority: 1,
+      },
+      "card-uuid-07": {
+        id: "card-uuid-07",
+        action: "U-Turn",
+        priority: 1,
+      },
+    },
+  },
+};
+
 export default function Practice() {
+  const [gameData, setGameData] = useState<PlayerGameData>(defaultGameData);
+
   return <StyledPractice>Test</StyledPractice>;
 }
