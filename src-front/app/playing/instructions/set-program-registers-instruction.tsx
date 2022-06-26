cce5f63529ab65d3534399b4d46a26a232095df7
Nick Ng <nicholas.cj.ng@gmail.com>
2022-06-26 14:15:43 +1200

    automatically select the first empty register if possible when programming robot

--- src-front/app/playing/instructions/set-program-registers-instruction.tsx
+++ src-front/app/playing/instructions/set-program-registers-instruction.tsx
@@ -4,13 +4,13 @@ export default function SetProgramRegistersInstruction() {
   return (
     <>
       <p>
-        Click on a programming card (Move 1, Rotate, etc.) and click on a
-        register to set it. You can click them in any order.
+        Select a register then choose on a programming card (Move 1, Rotate,
+        etc.) to set it. The first empty register will be selected automatically
+        but you can program them in any order.
       </p>
       <p>
         If you change your mind about a register, you can either set a new
-        instruction or clear it by clicking on it twice (first click selects,
-        second click clears)
+        instruction or clear it by clicking on it once it's selected.
       </p>
     </>
   );
