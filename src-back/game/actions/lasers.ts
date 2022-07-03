3581c212b42c8a90882135d2fd0a8ed80777e71d
Nick Ng <nicholas.cj.ng@gmail.com>
2022-07-03 17:01:27 +1200

    robots can shoot sideways

--- src-back/game/actions/lasers.ts
+++ src-back/game/actions/lasers.ts
@@ -1,12 +1,4 @@
-import {
-  Robot,
-  Map,
-  MapItem,
-  MainGameState,
-  FlagMapItem,
-  WallMapItem,
-  StraightConveyorMapItem,
-} from "../../../dist-common/game-types";
+import { Robot, MapItem, WallMapItem } from "../../../dist-common/game-types";
 import { damageRobot, isRobotDestroyed } from "./utils";
 
 const directionMap = {
@@ -16,7 +8,7 @@ const directionMap = {
   left: { xd: -1, yd: 0 },
 } as const;
 
-export const shootRobotLasersVertical = (
+export const shootRobotLasers = (
   robots: Robot[],
   mapItems: MapItem[]
 ): boolean => {
@@ -29,49 +21,65 @@ export const shootRobotLasersVertical = (
     const { position } = robot;
     const { xd, yd } = directionMap[position.facing];
 
-    if (["up", "down"].includes(position.facing)) {
-      const blockingWalls = mapItems.filter(
-        (mi) =>
-          mi.type === "wall" && mi.x === position.x && mi.x1 === position.x
-      );
-      const blockingRobots = robots.filter(
-        (r) =>
-          r.playerId !== robot.playerId &&
-          r.position.x === position.x &&
-          !isRobotDestroyed(r)
-      );
+    let parallel: "x" | "y" = "y";
+    let perpendicular: "x" | "y" = "x";
+    let paralleld: number = yd;
 
-      const blockingThings = blockingWalls
-        .map((wall) => ({
-          playerId: null as string | null,
-          x: wall.x,
-          y: (wall.y + (wall as WallMapItem).y1) * 0.5,
+    if (["left", "right"].includes(position.facing)) {
+      parallel = "x";
+      perpendicular = "y";
+      paralleld = xd;
+    }
+
+    const parallel1 = `${parallel}1` as const;
+    const perpendicular1 = `${perpendicular}1` as const;
+
+    const blockingWalls = mapItems.filter((mi) => {
+      return (
+        mi.type === "wall" &&
+        mi[perpendicular] === position[perpendicular] &&
+        mi[perpendicular1] === position[perpendicular]
+      );
+    });
+
+    const blockingRobots = robots.filter((r) => {
+      return (
+        r.playerId !== robot.playerId &&
+        r.position[perpendicular] === position[perpendicular] &&
+        !isRobotDestroyed(r)
+      );
+    });
+
+    type Thing = { playerId: string; x: number; y: number };
+    const blockingThings = blockingWalls
+      .map((wall) => ({
+        playerId: null as string | null,
+        [perpendicular]: wall[perpendicular],
+        [parallel]: (wall[parallel] + (wall as WallMapItem)[parallel1]) * 0.5,
+      }))
+      .concat(
+        blockingRobots.map((r) => ({
+          playerId: r.playerId,
+          [perpendicular]: r.position[perpendicular],
+          [parallel]: r.position[parallel],
         }))
-        .concat(
-          blockingRobots.map((r) => ({
-            playerId: r.playerId,
-            x: r.position.x,
-            y: r.position.y,
-          }))
-        )
-        .filter((a) => a.y * yd > position.y * yd)
-        .sort((a, b) => yd * (a.y - b.y));
+      )
+      .filter(
+        (a) =>
+          (a as Thing)[parallel] * paralleld > position[parallel] * paralleld
+      )
+      .sort(
+        (a, b) => paralleld * ((a as Thing)[parallel] - (b as Thing)[parallel])
+      );
 
-      if (blockingThings.length > 0) {
-        const targetRobot = robots.find(
-          (r) => r.playerId === blockingThings[0].playerId
-        );
+    if (blockingThings.length > 0) {
+      const targetRobot = robots.find(
+        (r) => r.playerId === blockingThings[0].playerId
+      );
 
-        if (targetRobot) {
-          shotsFired = true;
-          damageRobot(targetRobot);
-        }
-      }
-
-      if (robot.playerId === "player-1's-uuid") {
-        console.log("robot", robot);
-        console.log("xyd", { xd, yd });
-        console.log("blockingThings", blockingThings);
+      if (targetRobot) {
+        shotsFired = true;
+        damageRobot(targetRobot);
       }
     }
   }
