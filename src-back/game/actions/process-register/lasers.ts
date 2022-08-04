fbdd45a7650ba8e9ae583413ed8951bdb11e8be5
Nick Ng <nicholas.cj.ng@gmail.com>
2022-08-04 21:29:09 +1200

    remove unused code

--- src-back/game/actions/process-register/lasers.ts
+++ src-back/game/actions/process-register/lasers.ts
@@ -2,7 +2,7 @@ import type { Robot, MapItem, LaserMapItem } from "dist-common/game-types";
 
 import { getLaserTarget } from "../../../../dist-common/get-laser-target";
 
-import { damageRobot, isRobotDestroyed } from "../utils";
+import { damageRobot } from "../utils";
 
 export const shootRobotLasers = (
   robots: Robot[],
